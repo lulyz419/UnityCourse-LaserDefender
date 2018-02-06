5ffa13666f05de1813535ec15eec490bdc24204d
lulyz419 <lulyz419@hotmail.com>
2018-02-06 15:55:34 +0100

    1ºcommit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			print ("uparrow");
+		}
+		
+	
+	}
+}
